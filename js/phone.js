07644c5507819a12506407373ca61b2ad88827d4
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2024-06-02 06:23:07 +0600

    display phone dynamically on each card

---
+++ js/phone.js
@@ -0,0 +1,8 @@
+const loadPhone = async () =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await response.json();
+    const phones = data.data;
+    console.log(phones);
+}
+
+loadPhone();
