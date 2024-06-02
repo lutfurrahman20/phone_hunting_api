e9ed01192fce5aa6798e91f9c3246523acb032f9
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2024-06-02 07:59:17 +0600

    implement search functionality and display result

--- js/phone.js
+++ js/phone.js
@@ -1,8 +1,49 @@
-const loadPhone = async () =>{
-    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) =>{
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await response.json();
     const phones = data.data;
-    console.log(phones);
+    // console.log(phones);
+    displayPhones(phones);
 }
 
-loadPhone();
+const displayPhones = phones=>{
+    // console.log(phones)
+    const phoneContainer = document.getElementById('phone-container');
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
+
+    phones.forEach(phone =>{
+        console.log(phone)
+        // create element 
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+        // add the innerHTML 
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        
+        
+        
+        `;
+        // appendChild
+        phoneContainer.appendChild(phoneCard);
+
+    })
+ 
+}
+
+// handle search button
+const handleSearch = () =>{
+    const searchField = document.getElementById("search-field");
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText)
+}
+
+// loadPhone();
